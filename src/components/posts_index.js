c7804a331d767f4f4130b6eaf085f06182e4955b
Rafael <rafael.escoto@hotmail.com>
2017-09-14 17:43:00 -0500

    Routing Implemented

---
+++ src/components/posts_index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class PostsIndex extends Component {
+  render() {
+    return (
+      <div>
+        List of blog posts.
+      </div>
+    );
+  }
+}
+
+//export default ()=>{return <div>List of blog posts.</div>;};
